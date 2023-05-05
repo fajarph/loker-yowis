90b57bbc0ecd8164f3d45a8afadac3f94b180a52
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-05-05 18:53:00 +0700

    fixed again

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require("sequelize")
 require('dotenv').config()
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env
+const { NODE_ENV, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env
 
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: 'postgres',
-    connectionString: process.env.NODE_ENV || 'postgresql://postgres:<27fajar03>@localhost:5432/<info_loker_by_yowis>',
-    ssl: process.env.NODE_ENV ? true : false
-    
+    dialectOptions: {
+        ssl: NODE_ENV === 'production'
+    }
 })
 
 module.exports = db;
