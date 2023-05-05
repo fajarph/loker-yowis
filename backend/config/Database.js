d24cafd0b2f9ff03e9992879fefdaf9b2d953ac4
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-05-05 17:39:18 +0700

    update package

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const { NODE_ENV, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env
 
-const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD,{
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: 'postgres',
     dialectOptions: {
