dcf7fda674f6d5b476d126d32fc62147ce40f070
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-05-05 18:49:47 +0700

    fixed

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require("sequelize")
 require('dotenv').config()
 
-const { NODE_ENV, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env
 
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: NODE_ENV === 'production'
-    }
+    connectionString: process.env.NODE_ENV || 'postgresql://postgres:<27fajar03>@localhost:5432/<info_loker_by_yowis>',
+    ssl: process.env.NODE_ENV ? true : false
+    
 })
 
 module.exports = db;
