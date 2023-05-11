7a33df26af3786c3965c92bb1de77493b8ef8c6d
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-05-11 20:41:12 +0700

    Update job.js

--- backend/routes/job.js
+++ backend/routes/job.js
@@ -6,7 +6,7 @@ const router = Router()
 
 router.get('/jobs',middleware.verifyUser, controllers.getJobs)
 router.get('/jobs/:id',middleware.verifyUser, controllers.getJobById)
-router.post('/jobs', validation.createJob, middleware.verifyUser, middleware.adminOnly, controllers.createJob)
+router.post('/jobs', validation.createJob, middleware.verifyUser, controllers.createJob)
 router.patch('/jobs/:id', validation.createJob, middleware.verifyUser, middleware.adminOnly, controllers.updateJob)
 router.delete('/jobs/:id', middleware.verifyUser, middleware.adminOnly, controllers.deleteJob)
 router.get('/jobsbyroleid', controllers.getJobsByRoleId)
